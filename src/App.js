7f01f5b8a6353e0f42b02fc887e9febe2ca81f46
borito1907 <boranerol03@gmail.com>
2022-11-06 18:53:47 -0800

    Deleted the Login.js file.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from "react";
 import Navbar from './components/Navbar/Navbar';
+
 // use later for navigation
 import {BrowserRouter as Router} from "react-router-dom";
 
