f837c05577f5b6b76b7b6dd0d4d0145c5472113b
Kosta Gjorgjievski <kostagjorgjievski@Kostas-MacBook-Air.local>
2022-11-11 12:19:16 -0800

    Implemented Firebase and Authentication system with SingUp and SingIn page.
    New Features
    - Added Create an Account Capabilites, new file SignUp.jsx
    - Added Log in an Aiccount Capabilities, new file Singin.jsx
    - Added view your Account(email) Capabilites, new file Account.jsx
    - Added the new routes to App.js, modified App.js
    - Implemented Tailwind to the Project for styiling 'tailwind.config.js'
    - Connected the application to Firebase, modified firebase.js, added AuthContext.js
    - Implemented the feature of PrivateRoutes, new file ProtectedRoute.js

--- src/App.js
+++ src/App.js
@@ -7,19 +7,33 @@ import Navbar from './components/Navbar/Navbar';
 import HomePage from './pages/Home';
 import ChatRoomsPage from './pages/ChatRooms';
 import ProfilePage from './pages/Profile';
-import SignInPage from './pages/SignIn';
+
+// Authentication
+import { AuthContextProvider } from './components/Authentication/context/AuthContext';
+import ProtectedRoute from './components/Authentication/ProtectedRoute';
+import SignIn from './components/Authentication/Signin';
+import Signup from './components/Authentication/Signup';
+import Account from './components/Authentication/Account';
 
 class App extends React.Component{
   render(){
     return (
       <div className="App">
         <div><Navbar /></div>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/chatrooms" element={<ChatRoomsPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/signin" element={<SignInPage />} />
-        </Routes>
+          <AuthContextProvider>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/chatrooms" element={<ChatRoomsPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path='/signup' element={<Signup />} />
+              <Route path='/account' element={
+                <ProtectedRoute>
+                  <Account />
+                </ProtectedRoute>
+                }/>
+            </Routes>
+          </AuthContextProvider>
       </div>
     );
   }  
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyCaHAN2nzMTfcJrHYnlX5AvolrdUtEfyU8",
     authDomain: "only-swipes.firebaseapp.com",
@@ -16,5 +17,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-// Firebase SDK uses this to connect to your backend
-const firebaseApp = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app
+
+
+
