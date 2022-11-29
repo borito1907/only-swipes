5614dccad6c2305e6ea03b46b42e6c5379a564e4
borito1907 <boranerol03@gmail.com>
2022-11-29 15:53:51 -0800

    added chakra-ui to npm

--- src/index.js
+++ src/index.js
@@ -3,20 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<<<<<<< HEAD
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
-);
-=======
   <React.StrictMode>
       <App />
   </React.StrictMode>
 );
 
 
->>>>>>> 7bc02f26ba45a92144804b08c3ce7e397aaac149
