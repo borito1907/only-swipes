7bc02f26ba45a92144804b08c3ce7e397aaac149
nitin-veera <nitinveera@ucla.edu>
2022-11-29 14:10:46 -0800

    created new firebase project and reconfigured firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -9,13 +9,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCaHAN2nzMTfcJrHYnlX5AvolrdUtEfyU8",
-    authDomain: "only-swipes.firebaseapp.com",
-    projectId: "only-swipes",
-    storageBucket: "only-swipes.appspot.com",
-    messagingSenderId: "465957444397",
-    appId: "1:465957444397:web:bc01e99712b0c81f7151a4",
-    measurementId: "G-BQ9W85QFKL"
+    apiKey: "AIzaSyA5XTM7rwOVor4OBiMQL7sdJzrLCki98Do",
+    authDomain: "only-swipes-v2.firebaseapp.com",
+    projectId: "only-swipes-v2",
+    storageBucket: "only-swipes-v2.appspot.com",
+    messagingSenderId: "599175594617",
+    appId: "1:599175594617:web:f71787f3a757f6e4be8aa8"
 };
 
 // Initialize Firebase
@@ -23,4 +22,5 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
+
 
--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -6,20 +6,21 @@ import CreateListing from '../components/feed-components/CreateListing'
 import { db } from "../lib/firebase.js";
 import { collection, getDocs } from "firebase/firestore";
 
+const listings = [{ "listerID": "2", "listingType": "Sell", "location": "Rende", "mealPeriod": "Lunch", "timePosted": "9:00" }];
+
 function Feed() {
 
+    // const [listings, setListings] = useState([]);
+    // const listingsCollectionRef = collection(db, "listings");
 
-    const [listings, setListings] = useState([]);
-    const listingsCollectionRef = collection(db, "listings");
+    // useEffect(() => {
+    //     const getListings = async () => {
+    //         const data = await getDocs(listingsCollectionRef);
+    //         setListings(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    //     };
 
-    useEffect(() => {
-        const getListings = async () => {
-            const data = await getDocs(listingsCollectionRef);
-            setListings(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-        };
-
-        getListings();
-    }, [listings]);
+    //     getListings();
+    // }, [listings]);
 
 
 
