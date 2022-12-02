962caf6a6102211708a8218fc9087765ce703500
Sahiti Gabrani <sahitigabrani@gmail.com>
2022-12-02 12:14:31 -0800

    border change when hover

--- src/components/ChatRooms/ChatItem.js
+++ src/components/ChatRooms/ChatItem.js
@@ -9,18 +9,27 @@ function ChatItem(props) {
     return (
         <ul className="ChatWith">
         <Box bg="#E6E6FA" ml="14" mr="10" my="0" width="sm" maxW="unset" maxH="sm" p="2" borderWidth="1px" borderRadius="md" 
+        _hover={{
+            border: "1px",
+            borderColor: "purple",
+
+        }}
+        
+        _focus={{
+            border: "1px",
+            borderColor: "black",
+
+        }}
+
         _active={{
             bg: '#dddfe2',
             transform: 'scale(0.98)',
             borderColor: "purple",
         }}
-        _focus={{
-            borderColor: "black",
 
-        }}
+        onClick={() => props.selectChat(props.id)}
 >
-        <li 
-        onClick={() => props.selectChat(props.id)}>
+        <li>
         
             {/* Might have to clear floats for this to work*/}
             {/* visit https://css-tricks.com/left-align-and-right-align-text-on-the-same-line/ */}
